39395a8f404782c543264e3dfb859de39fea8293
anniech1 <ach577@nyu.edu>
2022-09-03 14:49:45 -0400

    added var for chars and ask user function to generate prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,28 @@
 // Assignment code here
+
+var userSelectedArray = [];
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCase= ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numbers = ['0','1','2','3','4','5','6','7','8','9'];
+var specialChar = ['!','@','#','$','%','^','&','*','(',')'];
+
+
+
+
 function generatePassword(length) {
   var password = '';
-  var chars = '0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  let passwordLength = length;
+  var passwordLength = length;
+  for (var i = 0; i <= passwordLength; i++) {
+     var randomNumber = Math.floor(Math.random() * chars.length);
+     password += chars.substring(randomNumber, randomNumber +1);
+   }
+  document.getElementById("password").value = password;
+
+
+}
+
+function askUser (){
+  passwordLength = prompt("How long")
 }
 
 
