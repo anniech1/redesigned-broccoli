e10e9260f965fa70aae4bd6ccb6065db702f071a
anniech1 <ach577@nyu.edu>
2022-09-03 16:14:58 -0400

    added math.floor function for generating based on length

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,4 @@
 // Assignment code here
-
 var userSelected = [];
 var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var upperCase= ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
@@ -9,19 +8,21 @@ var specialChar = ['!','@','#','$','%','^','&','*','(',')'];
 
 
 
-function generatePassword(length) {
+function generatePassword() {
   var password = '';
-  var passwordLength = length;
   for (var i = 0; i <= passwordLength; i++) {
-     var randomNumber = Math.floor(Math.random() * chars.length);
-     password += chars.substring(randomNumber, randomNumber +1);
+     var randomNumber = Math.floor(Math.random() * userSelected.length);
+     password = password + userSelected[randomNumber];
    }
-  document.getElementById("password").value = password;
+   return password;
+  // document.getElementById("password").value = password;
 
 
 }
 
 function askUser (){
+  userSelected = [];
+
   passwordLength = parseInt(prompt("How long do you want your password to be? Select from 8-128 characters"));
     if(isNaN(passwordLength)|| passwordLength < 8 || passwordLength > 128) {
       alert("Incorrect password length")
@@ -40,6 +41,7 @@ function askUser (){
     if (confirm("Do you want to include special characters?")){
       userSelected = userSelected.concat(specialChar);
     }
+    return true;
 }
 
 
@@ -49,10 +51,14 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var userResults = askUser ();
+  
+  if (userResults) {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
+  }
 
 }
 
