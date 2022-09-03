8afc60588af37c5d6474214e2aab28e69a2e0f16
anniech1 <ach577@nyu.edu>
2022-09-03 15:20:39 -0400

    added prompt questions

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
 // Assignment code here
 
-var userSelectedArray = [];
+var userSelected = [];
 var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var upperCase= ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var numbers = ['0','1','2','3','4','5','6','7','8','9'];
@@ -22,7 +22,24 @@ function generatePassword(length) {
 }
 
 function askUser (){
-  passwordLength = prompt("How long")
+  passwordLength = parseInt(prompt("How long do you want your password to be? Select from 8-128 characters"));
+    if(isNaN(passwordLength)|| passwordLength < 8 || passwordLength > 128) {
+      alert("Incorrect password length")
+      return false;
+    }
+
+    if (confirm("Do you want to include lowercase characters?")){
+      userSelected = userSelected.concat(lowerCase);
+    }
+    if (confirm("Do you want to include uppercase characters?")){
+      userSelected = userSelected.concat(upperCase);
+    }
+    if (confirm("Do you want to include numbers?")){
+      userSelected = userSelected.concat(numbers);
+    }
+    if (confirm("Do you want to include special characters?")){
+      userSelected = userSelected.concat(specialChar);
+    }
 }
 
 
